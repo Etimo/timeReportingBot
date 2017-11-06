3e4e86e54189548b7e8d6dc0e6813cd3bcb08bd9
Nina M <manina103@hotmail.com>
2017-11-06 11:36:58 +0100

    added function to check if it should run

--- index.js
+++ index.js
@@ -14,53 +14,83 @@ const creds = require("./creds/SlackBot-0bd3a3481e0e.json");
 const googleSpreadsheet = require("google-spreadsheet");
 const googleSpreadsheetKey_1 = require("./creds/googleSpreadsheetKey");
 const moment = require("moment");
-const job = new cron_1.CronJob("0 0 10 * * *", () => {
+const job = new cron_1.CronJob("0 0 9 * * *", () => {
+    main();
+});
+function main() {
+    const momentNow = moment();
+    if (shouldRunAtMoment(momentNow) === false) {
+        return;
+    }
     const doc = new googleSpreadsheet(googleSpreadsheetKey_1.googleSpreadsheetKey);
     const thisDate = moment().date();
     const lastDayInMonth = moment().daysInMonth();
-    if (thisDate === lastDayInMonth || thisDate === 1) {
-        doc.useServiceAccountAuth(creds, (err) => {
-            doc.getInfo((error, info) => {
-                for (const worksheet of info.worksheets) {
-                    if (worksheet.title !== "TID: Planering och rapportering=>"
-                        && worksheet.title !== "Modellparametrar"
-                        && worksheet.title !== "Holidays") {
-                        const sheet = worksheet;
-                        const name = worksheet.title;
-                        const spreadsheet = new spreadsheet_1.default(sheet, name, doc);
-                        const callFuncs = () => __awaiter(this, void 0, void 0, function* () {
-                            try {
-                                const datesArr = yield spreadsheet.getRowColOfDates();
-                                let monthColNumbers = [];
-                                let datesWithoutWeekend = [];
-                                let workingDates = [];
-                                const holidaysArr = yield spreadsheet.checkHolidays();
-                                if (thisDate === 1) {
-                                    monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, true);
-                                    datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, true);
-                                    workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, true);
-                                }
-                                else {
-                                    monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, false);
-                                    datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, false);
-                                    workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, false);
-                                }
-                                const reportedTimeObj = yield spreadsheet.findRowOfReportedTime();
-                                const rowNumberOfReportedCells = spreadsheet.getRowNumberOfReportedTime(reportedTimeObj);
-                                const hourCells = yield spreadsheet.getTimeReported(rowNumberOfReportedCells, monthColNumbers);
-                                spreadsheet.checkTimeFilled(workingDates, hourCells);
+    doc.useServiceAccountAuth(creds, (err) => {
+        doc.getInfo((error, info) => {
+            for (const worksheet of info.worksheets) {
+                if (worksheet.title !== "TID: Planering och rapportering=>"
+                    && worksheet.title !== "Modellparametrar"
+                    && worksheet.title !== "Holidays") {
+                    const sheet = worksheet;
+                    const name = worksheet.title;
+                    const spreadsheet = new spreadsheet_1.default(sheet, name, doc);
+                    const callFuncs = () => __awaiter(this, void 0, void 0, function* () {
+                        try {
+                            const datesArr = yield spreadsheet.getRowColOfDates();
+                            let monthColNumbers = [];
+                            let datesWithoutWeekend = [];
+                            let workingDates = [];
+                            const holidaysArr = yield spreadsheet.checkHolidays();
+                            if (isFirstBusinessDayOfTheMonth(momentNow)) {
+                                monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, true);
+                                datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, true);
+                                workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, true);
                             }
-                            catch (err) {
-                                console.log(err);
+                            else {
+                                monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, false);
+                                datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, false);
+                                workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, false);
                             }
-                        });
-                        callFuncs();
-                    }
+                            const reportedTimeObj = yield spreadsheet.findRowOfReportedTime();
+                            const rowNumberOfReportedCells = spreadsheet.getRowNumberOfReportedTime(reportedTimeObj);
+                            const hourCells = yield spreadsheet.getTimeReported(rowNumberOfReportedCells, monthColNumbers);
+                            spreadsheet.checkTimeFilled(workingDates, hourCells);
+                        }
+                        catch (err) {
+                            console.log(err);
+                        }
+                    });
+                    callFuncs();
                 }
-            });
+            }
         });
+    });
+}
+function shouldRunAtMoment(momentArg) {
+    return isFirstBusinessDayOfTheMonth(momentArg) || isLastBusinessDayOfTheMonth(momentArg);
+}
+function isFirstBusinessDayOfTheMonth(momentArg) {
+    const firstBusinessDayOfMonth = getFirstBusinessDayOfMonth(momentArg);
+    return firstBusinessDayOfMonth.date() === momentArg.date();
+}
+function isLastBusinessDayOfTheMonth(momentArg) {
+    const lastBusinessDayOfMonth = getLastBusinessDayOfMonth(momentArg);
+    return lastBusinessDayOfMonth.date() === momentArg.date();
+}
+function getFirstBusinessDayOfMonth(momentArg) {
+    const momentToCheck = momentArg.clone().startOf("month");
+    while (momentToCheck.isoWeekday() > 5) {
+        momentToCheck.add(1, "days");
     }
-});
+    return momentToCheck;
+}
+function getLastBusinessDayOfMonth(momentArg) {
+    const momentToCheck = momentArg.clone().endOf("month");
+    while (momentToCheck.isoWeekday() > 5) {
+        momentToCheck.subtract(1, "days");
+    }
+    return momentToCheck;
+}
 // function x(err, holidaysArr) {
 //   // istället för callback, "wrapper" funktion som använder promise
 //   // om success: promise.resolve
--- index.ts
+++ index.ts
@@ -8,63 +8,103 @@ import {googleSpreadsheetKey} from "./creds/googleSpreadsheetKey";
 
 import * as moment from "moment";
 
-const job = new CronJob("0 0 10 * * *", () => {
+const job = new CronJob("0 0 9 * * *", () => {
+  main();
+});
+
+function main() {
+  const momentNow = moment();
+
+  if (shouldRunAtMoment(momentNow) === false) {
+    return;
+  }
 
   const doc: any = new googleSpreadsheet(googleSpreadsheetKey);
   const thisDate: number = moment().date();
   const lastDayInMonth = moment().daysInMonth();
 
-  if (thisDate === lastDayInMonth || thisDate === 1) {
+  doc.useServiceAccountAuth(creds, (err) => {
+    doc.getInfo((error, info) => {
+      for (const worksheet of info.worksheets) {
 
-    doc.useServiceAccountAuth(creds, (err) => {
-      doc.getInfo((error, info) => {
-        for (const worksheet of info.worksheets) {
+        if (worksheet.title !== "TID: Planering och rapportering=>"
+        && worksheet.title !== "Modellparametrar"
+        && worksheet.title !== "Holidays") {
+          const sheet: any = worksheet;
+          const name = worksheet.title;
+          const spreadsheet = new Spreadsheet(sheet, name, doc);
 
-          if (worksheet.title !== "TID: Planering och rapportering=>"
-          && worksheet.title !== "Modellparametrar"
-          && worksheet.title !== "Holidays") {
-            const sheet: any = worksheet;
-            const name = worksheet.title;
-            const spreadsheet = new Spreadsheet(sheet, name, doc);
+          const callFuncs = async () => {
 
-            const callFuncs = async () => {
+            try {
+              const datesArr: any = await spreadsheet.getRowColOfDates();
 
-              try {
-                const datesArr: any = await spreadsheet.getRowColOfDates();
+              let monthColNumbers: number[] = [];
+              let datesWithoutWeekend: any[] = [];
+              let workingDates: number[] = [];
 
-                let monthColNumbers: number[] = [];
-                let datesWithoutWeekend: any[] = [];
-                let workingDates: number[] = [];
+              const holidaysArr: any = await spreadsheet.checkHolidays();
 
-                const holidaysArr: any = await spreadsheet.checkHolidays();
+              if (isFirstBusinessDayOfTheMonth(momentNow)) {
+                monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, true);
+                datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, true);
+                workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, true);
+              }else {
+                monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, false);
+                datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, false);
+                workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, false);
+              }
 
-                if (thisDate === 1) {
-                  monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, true);
-                  datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, true);
-                  workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, true);
-                }else {
-                  monthColNumbers = spreadsheet.getSpecificMonthSpan(datesArr, false);
-                  datesWithoutWeekend = spreadsheet.getWeekdays(datesArr, false);
-                  workingDates = spreadsheet.workingDates(datesWithoutWeekend, holidaysArr, false);
-                }
-
-                const reportedTimeObj: any = await spreadsheet.findRowOfReportedTime();
-                const rowNumberOfReportedCells: number = spreadsheet.getRowNumberOfReportedTime(reportedTimeObj);
-                const hourCells: any = await spreadsheet.getTimeReported(rowNumberOfReportedCells, monthColNumbers);
-                spreadsheet.checkTimeFilled(workingDates, hourCells);
-              }catch (err) {
-                console.log(err);
-            }
-          };
-
-            callFuncs();
+              const reportedTimeObj: any = await spreadsheet.findRowOfReportedTime();
+              const rowNumberOfReportedCells: number = spreadsheet.getRowNumberOfReportedTime(reportedTimeObj);
+              const hourCells: any = await spreadsheet.getTimeReported(rowNumberOfReportedCells, monthColNumbers);
+              spreadsheet.checkTimeFilled(workingDates, hourCells);
+            }catch (err) {
+              console.log(err);
           }
-        }
+        };
 
-      });
+          callFuncs();
+        }
+      }
+
     });
+  });
+}
+
+function shouldRunAtMoment(momentArg) {
+  return isFirstBusinessDayOfTheMonth(momentArg) || isLastBusinessDayOfTheMonth(momentArg);
+}
+
+function isFirstBusinessDayOfTheMonth(momentArg) {
+  const firstBusinessDayOfMonth = getFirstBusinessDayOfMonth(momentArg);
+  return firstBusinessDayOfMonth.date() === momentArg.date();
+}
+
+function isLastBusinessDayOfTheMonth(momentArg) {
+  const lastBusinessDayOfMonth = getLastBusinessDayOfMonth(momentArg);
+  return lastBusinessDayOfMonth.date() === momentArg.date();
+}
+
+function getFirstBusinessDayOfMonth(momentArg) {
+  const momentToCheck = momentArg.clone().startOf("month");
+
+  while (momentToCheck.isoWeekday() > 5) {
+    momentToCheck.add(1, "days");
   }
-});
+
+  return momentToCheck;
+}
+
+function getLastBusinessDayOfMonth(momentArg) {
+  const momentToCheck = momentArg.clone().endOf("month");
+
+  while (momentToCheck.isoWeekday() > 5) {
+    momentToCheck.subtract(1, "days");
+  }
+
+  return momentToCheck;
+}
 
 // function x(err, holidaysArr) {
 //   // istället för callback, "wrapper" funktion som använder promise
